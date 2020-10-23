5d1f0f2c37be5b39301edc812eaaa06136eb6b51
gianluca.pasquali <gianluca.pasquali@IT-PASQUALI.emec.dom>
2020-10-23 17:09:25 +0200

    Cambio Directory

--- USB_Log_Insert/Debug/source/USB_Log.d
+++ USB_Log_Insert/Debug/source/USB_Log.d
@@ -1,103 +1,103 @@
 source/USB_Log.o source/USB_Log.d: ../source/USB_Log.c \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\sources/usb_host_config.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\usb\host/usb_host.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\usb\include/usb.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\drivers/fsl_common.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\CMSIS/fsl_device_registers.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\CMSIS/MK22D5.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\CMSIS/core_cm4.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\CMSIS/cmsis_version.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\CMSIS/cmsis_compiler.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\CMSIS/cmsis_gcc.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\CMSIS/system_MK22D5.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\CMSIS/MK22D5_features.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\drivers/fsl_clock.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\drivers/fsl_common.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\osa/usb_osa.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\osa/usb_osa_bm.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\usb\include/usb_misc.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\usb\include/usb_spec.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\usb\include/usb_misc.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\usb\include/usb_spec.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\usb\host/usb_host_framework.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\board/board.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\drivers/fsl_uart.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\drivers/fsl_port.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\drivers/fsl_flash.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\drivers/fsl_pit.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\board/peripherals.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\board/pin_mux.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\board/clock_config.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\CMSIS/MK22D5.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\utilities/fsl_debug_console.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\usb\host\class/usb_host_msd.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\sources/host_msd_fatfs.h \
- C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\drivers/fsl_gpio.h
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\sources/usb_host_config.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\usb\host/usb_host.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\usb\include/usb.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\drivers/fsl_common.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\CMSIS/fsl_device_registers.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\CMSIS/MK22D5.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\CMSIS/core_cm4.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\CMSIS/cmsis_version.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\CMSIS/cmsis_compiler.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\CMSIS/cmsis_gcc.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\CMSIS/system_MK22D5.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\CMSIS/MK22D5_features.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\drivers/fsl_clock.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\drivers/fsl_common.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\osa/usb_osa.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\osa/usb_osa_bm.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\usb\include/usb_misc.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\usb\include/usb_spec.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\usb\include/usb_misc.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\usb\include/usb_spec.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\usb\host/usb_host_framework.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\board/board.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\drivers/fsl_uart.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\drivers/fsl_port.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\drivers/fsl_flash.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\drivers/fsl_pit.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\board/peripherals.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\board/pin_mux.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\board/clock_config.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\CMSIS/MK22D5.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\utilities/fsl_debug_console.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\usb\host\class/usb_host_msd.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\sources/host_msd_fatfs.h \
+ C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\drivers/fsl_gpio.h
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\sources/usb_host_config.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\sources/usb_host_config.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\usb\host/usb_host.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\usb\host/usb_host.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\usb\include/usb.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\usb\include/usb.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\drivers/fsl_common.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\drivers/fsl_common.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\CMSIS/fsl_device_registers.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\CMSIS/fsl_device_registers.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\CMSIS/MK22D5.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\CMSIS/MK22D5.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\CMSIS/core_cm4.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\CMSIS/core_cm4.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\CMSIS/cmsis_version.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\CMSIS/cmsis_version.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\CMSIS/cmsis_compiler.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\CMSIS/cmsis_compiler.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\CMSIS/cmsis_gcc.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\CMSIS/cmsis_gcc.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\CMSIS/system_MK22D5.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\CMSIS/system_MK22D5.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\CMSIS/MK22D5_features.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\CMSIS/MK22D5_features.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\drivers/fsl_clock.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\drivers/fsl_clock.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\drivers/fsl_common.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\drivers/fsl_common.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\osa/usb_osa.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\osa/usb_osa.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\osa/usb_osa_bm.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\osa/usb_osa_bm.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\usb\include/usb_misc.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\usb\include/usb_misc.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\usb\include/usb_spec.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\usb\include/usb_spec.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\usb\include/usb_misc.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\usb\include/usb_misc.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\usb\include/usb_spec.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\usb\include/usb_spec.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\usb\host/usb_host_framework.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\usb\host/usb_host_framework.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\board/board.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\board/board.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\drivers/fsl_uart.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\drivers/fsl_uart.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\drivers/fsl_port.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\drivers/fsl_port.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\drivers/fsl_flash.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\drivers/fsl_flash.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\drivers/fsl_pit.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\drivers/fsl_pit.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\board/peripherals.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\board/peripherals.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\board/pin_mux.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\board/pin_mux.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\board/clock_config.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\board/clock_config.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\CMSIS/MK22D5.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\CMSIS/MK22D5.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\utilities/fsl_debug_console.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\utilities/fsl_debug_console.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\usb\host\class/usb_host_msd.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\usb\host\class/usb_host_msd.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\sources/host_msd_fatfs.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\sources/host_msd_fatfs.h:
 
-C:\Users\gianluca.pasquali\Documents\MCUXpressoIDE_11.1.1_3241\workspace\USB_Log_Insert\USB_Log_Insert\drivers/fsl_gpio.h:
+C:\Users\gianluca.pasquali\git\repository\USB_Log_Insert\drivers/fsl_gpio.h:
